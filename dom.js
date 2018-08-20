9c76ca3c234e1d72ad2fcba12433244b57868578
W0LFYY <ethanmadrillrivera@gmail.com>
2018-08-20 14:42:40 -0600

    initial rps commit

---
+++ dom.js
@@ -0,0 +1,4 @@
+var rock = document.getElementById('rock')
+var paper = document.getElementById('paper')
+var scissors = document.getElementById('scissors')
+var computerChoices = ["rock", "paper", "scissors"];
